429e06452b8f3bf994e427781d68ac182e5d57a9
Pardeep Dhull <dhull007@gmail.com>
2021-02-02 18:57:57 -0500

    Update SQL_CIS_Benchmark.ps1

--- SQL_CIS_Benchmark.ps1
+++ SQL_CIS_Benchmark.ps1
@@ -5,7 +5,9 @@
 #                Written by Ian Guile                #
 #                                                    #
 ######################################################
-#02022021 - PD - Got this script working on SQl 2019 version; it should be used as an algorithem templete instead of fix all code. Its just technical refactoring to get it execute without errors.
+#02022021 - PD - Got this script working on SQl 2019 version; it should be used as an algorithem templete instead of fix all code. 
+#Its just technical refactoring to get it execute without errors.
+
 $Fix_Allowed = 0
 
  
